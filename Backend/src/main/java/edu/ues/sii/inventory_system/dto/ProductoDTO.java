6b9ff0a7b2336d7026c4df01e953a0fd5fccc30d
Grupo4 <grupo4@gmail.com>
2026-04-23 10:52:20 -0600

    fix: Correcciones en Mensajes de Validacion

--- Backend/src/main/java/edu/ues/sii/inventory_system/dto/ProductoDTO.java
+++ Backend/src/main/java/edu/ues/sii/inventory_system/dto/ProductoDTO.java
@@ -11,27 +11,27 @@ public class ProductoDTO {
             message = "El nombre solo debe contener letras")
     @NotBlank(message = "El nombre no puede estar vacío")
     @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
-    @Schema(example="Ej. Laptop")
+    @Schema(example="-")
     private String nombre;
 
 
     @NotNull(message = "El precio es obligatorio")
     @DecimalMin(value = "0.01", message = "El precio debe ser mayor a 0")
     @Digits(integer = 10, fraction = 2, message = "Formato de precio inválido")
-    @Schema(example="Ej. 150.55")
+    @Schema(example="0.00")
     private Double precio;
 
     @NotNull(message = "El stock es obligatorio")
-    @Min(value = 0, message = "El stock no puede ser negativo")
+    @Min(value = 1, message = "El stock no puede ser negativo o Cero")
     @Max(value = 100000, message = "Stock demasiado alto")
-    @Schema(example="Ej. 55")
+    @Schema(example="0")
     private Integer stock;
 
     @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9 ,.-]+$",
             message = "La descripción contiene caracteres inválidos")
     @NotBlank(message = "La descripción no puede estar vacía")
     @Size(min = 5, max = 255, message = "La descripción debe tener entre 5 y 255 caracteres")
-    @Schema(example="Ej. Breve Resumen...")
+    @Schema(example="-")
     private String descripcion;
 
     //GETTER Y SETTER
